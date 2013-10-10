d0bba31ea638efb81154e667437fa790110d2cb5
The Mini John <admin@phoenix-prime.gr>
2013-10-10 23:12:30 +0300

    Authentication Fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 	impressionist :actions => [:show]
 
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!
+	before_action :authenticate_user!, :except => [:index]
 
 
   # GET /questions
