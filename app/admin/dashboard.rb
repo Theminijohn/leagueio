193c4e0efcb2ea33eadf4c999869bed8b19fe9d2
The Mini John <admin@phoenix-prime.gr>
2013-10-17 17:57:40 +0300

    Active Admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,36 +4,34 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
 
+
+		panel "Recent Users" do
+			table_for User.order("created_at desc").limit(10) do
+				column :id
+				column :email
+				column :user_name do |u|
+					link_to u.user_name, [:admin, u]
+				end
+				column :points
+				column :sign_in_count
+				column :created_at
+			end
+			strong { link_to "View all Users", admin_users_path }
+		end
+
 		panel "Recent Questions" do
 			table_for Question.order("created_at desc").limit(10) do
 				column :id
 				column :title do |q|
 					link_to q.title, [:admin, q]
 				end
-				column :body
+				column "Votes", :votes_count
 				column :user
+				column :created_at
 			end
 			strong { link_to "View all Questions", admin_questions_path }
 		end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
